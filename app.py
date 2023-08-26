f82687ed29a5c2836b375454d962f33469e3cdbf
rubo <gustavo.rubo@hotmail.com>
2023-08-26 00:06:58 -0300

    add webpage and API for search

---
+++ app.py
@@ -0,0 +1,36 @@
+import json
+import base64
+import numpy as np
+from os import path
+from flask import Flask, jsonify, request, render_template
+
+
+app = Flask(__name__)
+
+with open('database.json', 'r') as f:
+    db = json.load(f)
+    db = np.array(db)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'GET':
+        return render_template('index.html')
+    elif request.method == 'POST':
+
+        print('caiu no POST')
+
+        data = request.json
+        text = data['search']
+
+        res = []
+        if text != '':
+            # TODO: better search function
+            res = db[[str.lower(text) in str.lower(' '.join(d['ocr'])) for d in db]]
+
+            for r in res:
+                with open(path.join('stitched', 'z1', r['thumb_file']), 'rb') as f:
+                    thumb = f.read()
+                    r['thumb_data'] = str(base64.b64encode(thumb))
+
+        return jsonify(list(res))
