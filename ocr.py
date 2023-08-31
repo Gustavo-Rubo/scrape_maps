37e4090edc0e8b1f273c0f512cca7188c34607dd
rubo <gustavo.rubo@hotmail.com>
2023-08-31 20:05:44 -0300

    switch to easyocr

--- ocr.py
+++ ocr.py
@@ -1,25 +1,37 @@
 import json
 import time
-import pytesseract
+import random
+import easyocr
 import numpy as np
 from os import path
+from PIL import Image
 from glob import glob
-from multiprocessing import Pool
 
+easyocr_reader = easyocr.Reader(['pt', 'en'])
 reprocess_existing = False
 
 
 def ocr(file_og):
     file = path.split(file_og)[1]
-    panoid = file[:-44]
+    panoid = file.split('=')[0]
     source = 'google' if len(panoid) <= 23 else 'user'
-    long = float(file[:-5].split('_')[-2])
-    lat = float(file[:-5].split('_')[-1])
+    long = float(file[:-5].split('=')[1])
+    lat = float(file[:-5].split('=')[2])
 
-    res = pytesseract.image_to_string(
-        file_og, nice=-10, lang='por+eng').split('\n')
-    res = np.array(res)
-    res = res[[len(r) >= 5 for r in res]]
+    img = Image.open(file_og)
+    box1 = (0, 0, img.width//2, img.height)
+    box2 = (img.width//2, 0, img.width, img.height)
+    canvas_size = 2100
+
+    easyocr_res = easyocr_reader.readtext(np.array(img.crop(box1)),
+        batch_size=1, detail=0, text_threshold=.6, canvas_size=canvas_size)
+    easyocr_res.extend(easyocr_reader.readtext(np.array(img.crop(box1).rotate(-90, expand=1)),
+        batch_size=1, detail=0, text_threshold=.6, canvas_size=canvas_size))
+    easyocr_res.extend(easyocr_reader.readtext(np.array(img.crop(box2)),
+        batch_size=1, detail=0, text_threshold=.6, canvas_size=canvas_size))
+    easyocr_res.extend(easyocr_reader.readtext(np.array(img.crop(box2).rotate(-90, expand=1)),
+        batch_size=1, detail=0, text_threshold=.6, canvas_size=canvas_size))
+
 
     return {
         'panoid': panoid,
@@ -27,21 +39,41 @@ def ocr(file_og):
         'long': long,
         'source': source,
         'thumb_file': file,
-        'ocr': list(res)
+        'ocr': easyocr_res
     }
 
 
 paths = glob(path.join('stitched', 'z5', '*'))
 paths.extend(glob(path.join('stitched', 'z4', '*')))
 
+old_data = []
+if not reprocess_existing:
+    with open('database.json', 'r') as f:
+        old_data = json.load(f)
+    new_paths = []
+    old_panoids = [d['panoid'] for d in old_data]
+    for p in paths:
+        panoid = path.split(p)[1].split('=')[0]
+        if panoid not in old_panoids:
+            new_paths.append(p)
+
+    paths = new_paths
+
+random.shuffle(paths)
+
 start = time.time()
-with Pool(12) as p:
-    data = p.map(ocr, paths)
+data = []
+batch = 800
+for i, p in enumerate(paths[:batch]):
+    print(f'ocr {i+1}/{len(paths[:batch])}')
+    data.append(ocr(p))
 stop = time.time()
 
-print(f'performed ocr in {len(paths)} files')
+new_data = [*old_data, *data]
+
+print(f'performed ocr in {len(data)} files')
 print(f'run time: {stop - start:.0f}s')
 print(f'time per file: {(stop - start)/len(paths):.2f}s')
 
 with open('database.json', 'w') as f:
-    json.dump(data, f)
+    json.dump(new_data, f)
--- stitch.py
+++ stitch.py
@@ -1,6 +1,6 @@
-from PIL import Image, ImageOps
 from os import path
 from glob import glob
+from PIL import Image, ImageOps
 
 restitch_existing = False
 
@@ -44,7 +44,7 @@ def merge_images_z1(imgs):
 
 for z in [1, 4, 5]:
     for i, folder in enumerate(glob(f'raw/z{z}/*')):
-        print(f'{i+1}/{len(glob(f"raw/z{z}/*"))}')
+        print(f'stitch {i+1}/{len(glob(f"raw/z{z}/*"))}')
         
         stitched_file_name = path.join('stitched', f'z{z}', f'{folder.split("/")[-1]}.jpeg')
 
@@ -60,12 +60,12 @@ for z in [1, 4, 5]:
             
             if z == 1:
                 if len(stitches) == 2:
-                    merge_images_z1(stitches).save(stitched_file_name, compress_level=7)
+                    merge_images_z1(stitches).save(stitched_file_name, compress_level=8)
             elif z == 4:
                 y = int(path.split(files[-1])[1][4:6])
                 x = int(path.split(files[-1])[1][8:10])
                 if len(stitches)  == (x+1)*(y-1):
-                    merge_images_z4(stitches, x+1, y-1).save(stitched_file_name, compress_level=7)
+                    merge_images_z4(stitches, x+1, y-1).save(stitched_file_name, compress_level=8)
             else:
                 if len(stitches) == 96:
-                    merge_images_z5(stitches).save(stitched_file_name, compress_level=7)
+                    merge_images_z5(stitches).save(stitched_file_name, compress_level=8)
