dcd615b435abad93adb6dc64d37817ab7acc584b
gustavorubo <gustavo.rubo@hotmail.com>
2024-03-15 17:11:28 -0300

    corrigir ocr_photo

--- ocr_photo.py
+++ ocr_photo.py
@@ -5,21 +5,41 @@ import easyocr
 import numpy as np
 from os import path
 from PIL import Image
+import PIL.ExifTags
 from glob import glob
 
 easyocr_reader = easyocr.Reader(['pt', 'en'])
 reprocess_existing = True
 
+def dms_to_dec(dms):
+    return float(dms[0])+float(dms[1])/60+float(dms[2])/3600
+
+def get_lat_long(img):
+    exif = {
+        PIL.ExifTags.TAGS[k]: v
+        for k, v in img._getexif().items()
+        if k in PIL.ExifTags.TAGS
+    }
+    lat = (-1 if exif['GPSInfo'][1] == 'S' else 1) * dms_to_dec(exif['GPSInfo'][2])
+    long = (-1 if exif['GPSInfo'][3] == 'W' else 1) * dms_to_dec(exif['GPSInfo'][4])
+
+    return lat, long
 
 def ocr(file_og):
     file = path.split(file_og)[1]
     panoid = file.split('=')[0]
     # source = 'google' if len(panoid) <= 23 else 'user'
-    long = float(file[:-5].split('=')[1])
-    lat = float(file[:-5].split('=')[2])
 
     img = Image.open(file_og)
     canvas_size = 2100
+    exif = {
+        PIL.ExifTags.TAGS[k]: v
+        for k, v in img._getexif().items()
+        if img._getexif() != None
+        and k in PIL.ExifTags.TAGS
+    }
+    lat = (-1 if exif['GPSInfo'][1] == 'S' else 1) * dms_to_dec(exif['GPSInfo'][2])
+    long = (-1 if exif['GPSInfo'][3] == 'W' else 1) * dms_to_dec(exif['GPSInfo'][4])
 
     easyocr_res = easyocr_reader.readtext(np.array(img),
         batch_size=1, detail=0, text_threshold=.6, canvas_size=canvas_size)
@@ -37,11 +57,12 @@ def ocr(file_og):
     }
 
 
-paths = glob(path.join('photos2', '*'))
+paths = glob(path.join('photos', '*'))
+paths = [path for path in paths if path[-4] == '.']
 
 old_data = []
 if not reprocess_existing:
-    with open('database_photos.json', 'r') as f:
+    with open(path.join('data', 'database_photos.json'), 'r') as f:
         old_data = json.load(f)
     new_paths = []
     old_panoids = [d['panoid'] for d in old_data]
@@ -71,5 +92,5 @@ print(f'performed ocr in {len(data)} files')
 print(f'run time: {stop - start:.0f}s')
 print(f'time per file: {(stop - start)/len(paths):.2f}s')
 
-with open('database_photos.json', 'w') as f:
+with open(path.join('data', 'database_photos.json'), 'w') as f:
     json.dump(new_data, f)
